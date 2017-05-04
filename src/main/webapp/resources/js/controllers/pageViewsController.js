8c904161809795e8a7f6102d14b4ac33b50b47ef
danielesperto <consulente.it15@percassi.it>
2017-05-04 11:56:54 +0200

    Chart wip

--- src/main/webapp/resources/js/controllers/pageViewsController.js
+++ src/main/webapp/resources/js/controllers/pageViewsController.js
@@ -2,58 +2,14 @@
  * 
  */
 var pageViewsController = function () {
-
     var _that = this;
-    var _data = {};
-
-    var params = {
-        fromDate: '2017-04-03',
-        toDate: '2017-04-13'
-    }
-
-    var _callAjax = function (type, url, successCbk) {
-        $.ajax({
-            url: 'endUser/pageview/',
-            dataType: 'json',
-            cache: false,
-            data: params,
-            async: false,
-            contentType: false,
-            processData: false,
-            type: 'GET',
-            beforeSend: function () {
-            },
-            success: function (res) {
-                if (res && res.monthly && res.monthly.length > 0) {
-                    _that._data = res;
-                } else {
-                    console.log("http resposonse is null");
-                }
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                console.error(JSON.stringify(jqXHR));
-            },
-            complete: function () {
-            }
-        });
-        return _that._data;
-    };
-
     var _setViewModel = function (vm) {
         _that.vm = vm;
-    };
-
-    var _getData = function () {
-        var data = _data;
-        return data;
     };
 
     return {
         setViewModel: function (vm) {
             _setViewModel(vm);
-        },
-        getData: function (url, callback) {
-            return _callAjax('GET', url, callback);
         }
     }
 }($);
