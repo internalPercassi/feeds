778d24923e32a50571701aba83b3f67aade85410
danielesperto <consulente.it15@percassi.it>
2017-05-29 15:50:57 +0200

    Date uploaded file

--- src/main/java/it/percassi/perparser/facade/model/UploadedFileModel.java
+++ src/main/java/it/percassi/perparser/facade/model/UploadedFileModel.java
@@ -2,8 +2,13 @@ package it.percassi.perparser.facade.model;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.bson.Document;
 
 /**
@@ -12,56 +17,58 @@ import org.bson.Document;
  */
 public class UploadedFileModel implements Serializable {
 
-	private String md5;
-	private String fileName;
-	private String type;
+    private final static Logger LOG = LogManager.getLogger(UploadedFileModel.class);
+
+    private String md5;
+    private String fileName;
+    private String type;
 //	@JsonSerialize(using = IsoDateSerializer.class)
-	private Date date;
+    private Date date;
 //	private Integer rowCount;
 
-	public UploadedFileModel() {
-	}
+    public UploadedFileModel() {
+    }
 
-	public UploadedFileModel(String fileName, byte[] bytes, String type) throws IOException {
-		String md5 = this.getMD5(bytes);
-		this.date = new Date();
-		this.md5 = md5;
+    public UploadedFileModel(String fileName, byte[] bytes, String type) throws IOException {
+        String md5 = this.getMD5(bytes);
+        this.date = new Date();
+        this.md5 = md5;
 //		this.rowCount = 0;
-		this.type = type;
-		this.fileName = fileName;
-	}
+        this.type = type;
+        this.fileName = fileName;
+    }
 
-	public String getFileName() {
-		return fileName;
-	}
+    public String getFileName() {
+        return fileName;
+    }
 
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
 
-	public String getMd5() {
-		return md5;
-	}
+    public String getMd5() {
+        return md5;
+    }
 
-	public void setMd5(String md5) {
-		this.md5 = md5;
-	}
+    public void setMd5(String md5) {
+        this.md5 = md5;
+    }
 
-	public String getType() {
-		return type;
-	}
+    public String getType() {
+        return type;
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	public Date getDate() {
-		return date;
-	}
+    public Date getDate() {
+        return date;
+    }
 
-	public void setDate(Date date) {
-		this.date = date;
-	}
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
 //	public Integer getRowCount() {
 //		return rowCount;
@@ -70,20 +77,46 @@ public class UploadedFileModel implements Serializable {
 //	public void setRowCount(Integer rowCount) {
 //		this.rowCount = rowCount;
 //	}
+    private static String getMD5(byte[] bytes) throws IOException {
+        String md5 = DigestUtils.md5Hex(bytes);
+        return md5;
+    }
 
-	private static String getMD5(byte[] bytes) throws IOException {
-		String md5 = DigestUtils.md5Hex(bytes);
-		return md5;
-	}
+    private Date getUploadDate() {
+        Date ret = new Date();
+        try {
+            if (AppEnum.FileType.FACEBOOK.getCode().equalsIgnoreCase(this.type)) {
+                // es. nome file: KIK_YYYYMMDDhhmm
+                String inFileNameDate = this.fileName.substring(4);
+                DateFormat formatter = new SimpleDateFormat("yyyyMMddHHmm");
+                formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+                ret = formatter.parse(inFileNameDate);
+            } else if (AppEnum.FileType.GL.getCode().equalsIgnoreCase(this.type)) {
+                //GL_KIK_2016100102000309
+                String inFileNameDate = this.fileName.substring(7);
+                DateFormat formatter = new SimpleDateFormat("yyyyMMddHHmm");
+                formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+                ret = formatter.parse(inFileNameDate);
+            } else if (AppEnum.FileType.OS.getCode().equalsIgnoreCase(this.type)) {
+                //Hybris_OversellingExt_KIK_20170516060024.txt_20170516090921550
+                String inFileNameDate = this.fileName.substring(26);
+                DateFormat formatter = new SimpleDateFormat("yyyyMMddHHmm");
+                formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+                ret = formatter.parse(inFileNameDate);
+            }
+        } catch (Exception e) {
+            LOG.warn("Unable to get date from fileName for the file: ***"+this.fileName+"***");
+            ret = new Date();
+        }
+        return ret;
+    }
 
-	
-	public Document toBSONDoc() {
-		Document ret = new Document();		
-		ret.append("md5", this.md5);			
-		ret.append("fileName", this.fileName);
-		ret.append("type", this.type);
-		ret.append("date", this.date);
-//		ret.append("rowCount", this.rowCount);
-		return ret;
-	}
+    public Document toBSONDoc() {
+        Document ret = new Document();
+        ret.append("md5", this.md5);
+        ret.append("fileName", this.fileName);
+        ret.append("type", this.type);
+        ret.append("date", this.getUploadDate());
+        return ret;
+    }
 }
--- src/main/java/it/percassi/perparser/service/mongo/MongoService.java
+++ src/main/java/it/percassi/perparser/service/mongo/MongoService.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONArray;
